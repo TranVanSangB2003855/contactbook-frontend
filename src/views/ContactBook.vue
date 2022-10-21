d5e711e192354d1f5c4221d45a4c8cf7d6dbf309
tran-van-sang <tranvansang6922@gmail.com>
2022-10-21 12:04:58 +0700

    Tao trang chinh sua thong tin

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -30,6 +30,16 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link 
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
+
             </div>
         </div>
     </div>
@@ -110,7 +120,7 @@ export default {
         },
     },
     mounted() {
-        this.refreshList(); 
+        this.refreshList();
     },
 
 };
