70397d387eb536021b2aa7818dc81a56f6bc2c7c
tran-van-sang <tranvansang6922@gmail.com>
2022-10-30 13:30:48 +0700

    Tao trang cap nhat lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -27,9 +27,9 @@
             </label>
         </div>
         <div class="form-group">
-            <button class="btn btn-primary">Lưu</button>
+            <button class="btn btn-primary"><i class="fas fa-save"></i> Lưu</button>
             <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
-                Xóa
+                <i class="fas fa-trash"></i> Xóa
             </button>
         </div>
     </Form>
